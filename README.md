55d4c8425cd52c7cbe09a38158c93aa2087c8512
Evan <evandvance@gmail.com>
2024-04-16 21:20:36 -0500

    Wrote script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cyb440-module-3-tool
+
+run `python3 ./main.py` and view the result
--- main.py
+++ main.py
@@ -36,27 +36,64 @@ def run_command(command:list[str]) -> str:
 
 
 def gen_keypair():
-    pass
+    """Generates an RSA keypair"""
+    run_command(["openssl", "genpkey", "-algorithm", "RSA", "-out", "private_key.pem", "-pkeyopt", "rsa_keygen_bits:2048"])
+    run_command(["openssl", "rsa", "-pubout", "-in", "private_key.pem", "-out", "public_key.pem"])
 
 
-def encrypt():
-    pass
+def encrypt(in_file:str):
+    """Encrypts a file using openssl
+
+    Args:
+        in_file (str): The file to be encrypted (use small files)
+    """
+    run_command(["openssl", "rsautl", "-encrypt", "-inkey", "public_key.pem", "-pubin", "-in", in_file, "-out", in_file.replace(".txt",".enc")])
 
 
-def decrypt():
-    pass
+def decrypt(in_file:str):
+    """Decrypts a file using openssl
+
+    Args:
+        in_file (str): The file to be decrypted
+    """
+    run_command(["openssl", "rsautl", "-decrypt", "-inkey", "private_key.pem", "-in", in_file, "-out", in_file.replace(".enc", ".dec")])
 
 
-def ec_sign():
-    pass
+def ec_sign(in_file:str):
+    """A function to sign files using openssl
+
+    Args:
+        in_file (str): the file to sign
+    """
+    run_command(["openssl", "dgst", "-sha256", "-sign", "private_key.pem", "-out", in_file + ".sig", in_file])
 
 
-def ec_verify():
-    pass
+def ec_verify(in_file:str):
+    """A function to verify a signature using openssl
+
+    Args:
+        in_file (str): File to verify
+    """
+    result = run_command(["openssl", "dgst", "-sha256", "-verify", "public_key.pem", "-signature", in_file.replace(".dec",".txt") + ".sig", in_file])
+
+    with open("./verified.txt", "w") as file:
+        file.write(result)
 
 
 def main():
-    pass
+    if not is_tool("openssl"):
+        print("Install openssl to continue")
+        exit()
+
+    with open("./example.txt", "w") as plaintext:
+        plaintext.write("Example Text")
+
+    gen_keypair()
+
+    ec_sign("./example.txt")
+    encrypt("./example.txt")
+    decrypt("./example.enc")
+    ec_verify("./example.dec")
 
 if __name__ == "__main__":
     main()
